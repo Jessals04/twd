a34e5e59e872049ef27bba1a58ff716e914c6602
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2023-02-03 10:46:47 +1100

    Added More Link Animations

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,8 +8,8 @@ export default function NavBar() {
           <div className='flex flex-row justify-between px-10 py-7 bg-grey'>
             <div className='font-AbrilFatface text-3xl my-auto'><Link className='' href={""}>T.W.D.</Link></div>
             <div className='font-PTSans flex flex-row gap-20'>
-                <Link className='my-auto aPU' href={""}>Home</Link>
-                <Link className='my-auto aPU' href={""}>Services</Link>
+                <Link className='my-auto aHScU' href={""}>Home</Link>
+                <Link className='my-auto aHScU' href={""}>Services</Link>
                 <Link className='bg-charcoal text-white text-lg font-bold py-1 px-2 rounded-lg' href={""}>Enquire Now</Link>
             </div>
           </div>
